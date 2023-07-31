cd35d2b28a9aedb0e442804bb80b5b3c588f6f6b
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-07-31 14:36:58 -0600

    test: Passing for happy path

--- app/controllers/api/v0/subscriptions_controller.rb
+++ app/controllers/api/v0/subscriptions_controller.rb
@@ -1,8 +1,15 @@
 class Api::V0::SubscriptionsController < ApplicationController
   rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_response
+
   def create
     @subscription = Subscription.create!(subscription_params)
     render json: SubscriptionSerializer.new(@subscription), status: 201
+  end
+
+  def destroy
+    @subscription = Subscription.find_by_id!(params[:id])
+    @subscription.change_status_to_cancelled
+    render json: SubscriptionSerializer.new(@subscription), status: 200
   end
   
   def render_invalid_response(error)
