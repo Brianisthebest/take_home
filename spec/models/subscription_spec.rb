9184e06fa8f9ae7dd75ff5e12ee1ba4f85b4ea96
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-07-31 14:36:44 -0600

    test: Add test for request and for model method to update status

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -9,4 +9,18 @@ RSpec.describe Subscription, type: :model do
     it { should validate_presence_of :status }
     it { should validate_presence_of :frequency }
   end
+
+  describe 'instance methods' do
+    describe 'change_status_to_cancelled' do
+      it 'changes the status of the subscription to cancelled' do
+        customer = create(:customer)
+        tea = create(:tea)
+        subscription = create(:subscription, status: 'Active', customer_id: customer.id, tea_id: tea.id)
+
+        expect(subscription.status).to eq('Active')
+        subscription.change_status_to_cancelled
+        expect(subscription.status).to eq('Cancelled')
+      end
+    end
+  end
 end
---
+++ spec/requests/api/v0/delete_subscription_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'DELETE /api/v0/subscriptions/:id', type: :request do
+  describe 'happy path' do
+    it 'changes the subscription to cancelled' do
+      customer = create(:customer)
+      tea = create(:tea)
+      subscription = create(:subscription, customer_id: customer.id, tea_id: tea.id)
+
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+
+      delete "/api/v0/subscriptions/#{subscription.id}", headers: headers
+      
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      expect(json[:data][:id]).to eq(subscription.id.to_s)
+      expect(json[:data][:type]).to eq('subscription')
+      expect(json[:data][:attributes][:customer_id]).to eq(subscription.customer_id)
+      expect(json[:data][:attributes][:tea_id]).to eq(subscription.tea_id)
+      expect(json[:data][:attributes][:title]).to eq(subscription.title)
+      expect(json[:data][:attributes][:price]).to eq(subscription.price)
+      expect(json[:data][:attributes][:status]).to eq('Cancelled')
+      expect(json[:data][:attributes][:frequency]).to eq(subscription.frequency)
+    end
+  end
+end
